7e5fb755c3f918638c21e2a5de655a0decdb77a8
mikaelekstrom <mikael.ekstrom@claremont.se>
2019-10-07 18:09:35 +0200

    Initial commit

---
+++ EntityItemTable.cs
@@ -0,0 +1,72 @@
+﻿using System.Xml.Serialization;
+
+namespace RestAPIImageRetriever
+{
+    [XmlRoot(ElementName = "object")]
+    public class Object
+    {
+        [XmlElement(ElementName = "id")]
+        public string Id { get; set; }
+        [XmlElement(ElementName = "entityId")]
+        public string EntityId { get; set; }
+    }
+
+    [XmlRoot(ElementName = "value")]
+    public class Value
+    {
+        [XmlAttribute(AttributeName = "xsi", Namespace = "http://www.w3.org/2000/xmlns/")]
+        public string Xsi { get; set; }
+        [XmlAttribute(AttributeName = "xs", Namespace = "http://www.w3.org/2000/xmlns/")]
+        public string Xs { get; set; }
+        [XmlAttribute(AttributeName = "type", Namespace = "http://www.w3.org/2001/XMLSchema-instance")]
+        public string Type { get; set; }
+        [XmlText]
+        public string Text { get; set; }
+    }
+
+    [XmlRoot(ElementName = "values")]
+    public class Values
+    {
+        [XmlElement(ElementName = "value")]
+        public Value Value { get; set; }
+    }
+
+    [XmlRoot(ElementName = "row")]
+    public class Row
+    {
+        [XmlElement(ElementName = "object")]
+        public Object Object { get; set; }
+        [XmlElement(ElementName = "values")]
+        public Values Values { get; set; }
+    }
+
+    [XmlRoot(ElementName = "rows")]
+    public class Rows
+    {
+        [XmlElement(ElementName = "row")]
+        public Row Row { get; set; }
+    }
+
+    [XmlRoot(ElementName = "entityItemTable")]
+    public class EntityItemTable
+    {
+        [XmlElement(ElementName = "cacheId")]
+        public string CacheId { get; set; }
+        [XmlElement(ElementName = "entityIdentifier")]
+        public string EntityIdentifier { get; set; }
+        [XmlElement(ElementName = "totalSize")]
+        public string TotalSize { get; set; }
+        [XmlElement(ElementName = "startIndex")]
+        public string StartIndex { get; set; }
+        [XmlElement(ElementName = "pageSize")]
+        public string PageSize { get; set; }
+        [XmlElement(ElementName = "rowCount")]
+        public string RowCount { get; set; }
+        [XmlElement(ElementName = "columnCount")]
+        public string ColumnCount { get; set; }
+        [XmlElement(ElementName = "columns")]
+        public string Columns { get; set; }
+        [XmlElement(ElementName = "rows")]
+        public Rows Rows { get; set; }
+    }
+}
---
+++ RestAPIImageRetriever.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Configuration;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using ImageResizer.Configuration;
+using ImageResizer.Plugins;
+using Newtonsoft.Json;
+
+namespace RestAPIImageRetriever
+{
+    public class RestAPIImageRetriever : IPlugin, IVirtualImageProvider
+    {
+        public IPlugin Install(Config c)
+        {
+            c.Plugins.add_plugin(this);
+            return this;
+        }
+
+        public bool Uninstall(Config c)
+        {
+            c.Plugins.remove_plugin(this);
+            return true;
+        }
+
+        private void LogFileAppender(string log)
+        {
+            var logFileName = "C:\\temp\\RestAPIImageRetriever.txt";
+            File.AppendAllLines(logFileName, new List<string> { $"{DateTime.UtcNow} :: {log}" });
+        }
+
+
+        static readonly HttpClient client = new HttpClient();
+
+        static readonly string API_BASE = "rest/V1.0/list/MediaAsset/bySearch?query=MediaAssetDocument.Identifier(eng,originalimage) EQUALS ";
+
+        public RestAPIImageRetriever()
+        {
+            try
+            {
+                client.BaseAddress = new Uri(ConfigurationManager.AppSettings["RestAPIBaseURI"]);
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                LogFileAppender("Plugin has started!");
+                var authString = $"{ConfigurationManager.AppSettings["RestAPIUser"]}:{ConfigurationManager.AppSettings["RestAPIPassword"]}";
+                var byteArray = Encoding.ASCII.GetBytes(authString);
+                client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Basic", Convert.ToBase64String(byteArray));
+
+                LogFileAppender("Plugin has started!");
+            }
+            catch (Exception e)
+            {
+                LogFileAppender($"Error thrown during startup, {e}");
+            }  
+        }
+
+        public bool FileExists(string virtualPath, NameValueCollection queryString)
+        {
+            LogFileAppender($"trying to see if file {virtualPath} exists");
+            var data = GetDataFromAPI(virtualPath);
+            LogFileAppender($"{virtualPath} file exists: {data.RowCount == "1"}");
+            return data.RowCount == "1";
+        }
+
+        private EntityItemTable GetDataFromAPI(string virtualPath)
+        {
+            var response = client.GetAsync(API_BASE + virtualPath.Split('/').ToList().Last()).Result;
+            LogFileAppender($"{response.Headers.Location}");
+            return JsonConvert.DeserializeObject<EntityItemTable>(response.Content.ToString());
+        }
+
+        public IVirtualFile GetFile(string virtualPath, NameValueCollection queryString)
+        {
+            var data = GetDataFromAPI(virtualPath);
+            return new BasicVirtualFile(virtualPath, data.Rows.Row.Values.Value.Text);
+        }
+    }
+
+    public class BasicVirtualFile : IVirtualFile
+    {
+        private readonly string url;
+
+        public string VirtualPath { get; }
+
+        public BasicVirtualFile(string virtualPath, string url)
+        {
+            if (string.IsNullOrWhiteSpace(virtualPath))
+            {
+                throw new ArgumentException("message", nameof(virtualPath));
+            }
+            VirtualPath = virtualPath;
+            this.url = url;
+        }
+
+        public Stream Open()
+        {
+            byte[] fileData = null;
+            using (var wc = new System.Net.WebClient())
+                fileData = wc.DownloadData(url);
+
+            return new MemoryStream(fileData);
+        }
+    }
+}
